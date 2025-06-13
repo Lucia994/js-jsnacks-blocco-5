be6e247040ddb57a52ed96b79a6f0f2ab6abcb27
Lucia994 <l.summa94@gmail.com>
2025-06-13 15:53:43 +0200

    Complete first exercise on forEach loop

---
+++ snack-1/script.js
@@ -0,0 +1,6 @@
+const names = ['Edoardo', 'Simone', 'Francesco'];
+
+names.forEach((nome, i, array) => {});
+
+ // Stampa in console tutti i nomi
+    console.log(names); // Risultato: 'Edoardo', 'Simone', 'Francesco'
