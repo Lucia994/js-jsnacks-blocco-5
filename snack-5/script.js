e128f9cf315f1272c795c5442b59f579808d6909
Lucia994 <l.summa94@gmail.com>
2025-06-13 18:29:05 +0200

    Complete fifth exercise

--- snack-5/script.js
+++ snack-5/script.js
@@ -1,4 +1,12 @@
 const nums = [2, 8, 4, 7, 12, 87];
 
-// Crea un array a partire dall'array nums, che abbia solo i numeri pari
+// Crea un array a partire dall'array nums, che abbia solo i numeri pari 
+const even = nums.filter((notOdd) => {
+    if (notOdd % 2 === 0) {
+        return true
+
+    } else return false
+ 
+})
+console.log(even);
 // Risultato: [2, 8, 4, 12]
